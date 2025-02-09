d0f6314faf0ac8a5593de1f9e35e760ba8489aed
mebusy <golden_slime@hotmail.com>
2025-02-09 22:20:37 +0800

    * 0.2

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'github/copilot.vim',
+    -- extra installation: `./install_gadget.py --all`
+    'puremourning/vimspector',
+    'tpope/vim-surround',
+    'vim-airline/vim-airline',
+  },
+}
