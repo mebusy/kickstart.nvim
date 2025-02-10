bf0f4c10038fbac072b061a92468c0135ff83e0d
mebusy <golden_slime@hotmail.com>
2025-02-11 00:08:00 +0800

    m

--- init.lua
+++ init.lua
@@ -235,6 +235,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+-- load extra vim script
 vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/config.vim')
 
 -- [[ Configure and install plugins ]]
