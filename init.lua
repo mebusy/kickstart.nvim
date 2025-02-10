eb74b08d73b09e4ff0734020cdcfc9bbd509a1ef
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-02-10 14:00:45 +0800

    * python/js format

--- init.lua
+++ init.lua
@@ -733,9 +733,11 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
+        python = { 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
       },
     },
   },
