2b64721ab965bf8932c4fc2136518bbef83bffc8
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-02-18 16:26:07 +0800

    *nvim-tree auto adjust board

--- lua/custom/plugins/tools.lua
+++ lua/custom/plugins/tools.lua
@@ -24,6 +24,7 @@ return {
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
       require('nvim-tree').setup {
+        view = { adaptive_size = true },
         renderer = {
           icons = {
             glyphs = {
