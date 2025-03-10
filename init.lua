0b81ac216fee3f31562f37a1927ba11cb1db35c3
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-03-10 14:29:11 +0800

    m

--- init.lua
+++ init.lua
@@ -684,7 +684,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- clangd to specify cpp std version:
+        -- method 1
+        --    .clangd   in project root
+        --    ```
+        --    CompileFlags:
+        --      Add: [-std=c++20]
+        --    ```
         -- clangd = {},
+        --
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
