51e64005d2fd62493816322167e32011e88e92a5
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2025-05-28 19:03:09 +0800

    Revert "* switch plugin order"
    This reverts commit acde2c74b851784c74ecf3a0f60782a05c39257a.

--- init.lua
+++ init.lua
@@ -522,8 +522,8 @@ require('lazy').setup({
     },
   },
   {
-    'mason-org/mason-lspconfig.nvim',
     -- Main LSP Configuration
+    'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
@@ -531,8 +531,7 @@ require('lazy').setup({
       -- { 'mason-org/mason.nvim', opts = {}, version = '^1.0.0' },
       -- { 'mason-org/mason-lspconfig.nvim', version = '^1.0.0' },
       { 'mason-org/mason.nvim', opts = {} }, -- nvim 0.11.1 fix this issue
-      'neovim/nvim-lspconfig',
-
+      { 'mason-org/mason-lspconfig.nvim' },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
