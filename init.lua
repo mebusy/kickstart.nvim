e2e1b713378a63320280a6754761c9601480443f
mebusy <golden_slime@hotmail.com>
2025-12-30 14:47:59 +0800

    m

--- init.lua
+++ init.lua
@@ -745,7 +745,8 @@ require('lazy').setup({
           -- code, if the language server you are using supports them
           --
           -- This may be unwanted, since they displace some of your code
-          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
+          -- if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
+          if client then
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
