025a839dc2e9d3411175698c464324f712e81f48
mebusy <golden_slime@hotmail.com>
2025-12-30 15:06:46 +0800

    m

--- init.lua
+++ init.lua
@@ -746,11 +746,17 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           -- if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
-          if client then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
+          -- if client then
+          --   map('<leader>th', function()
+          --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+          --   end, '[T]oggle Inlay [H]ints')
+          -- end
+          -- if client and client.server_capabilities and client.server_capabilities.inlayHintProvider then
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            print('inlay hint status: ' .. tostring(vim.lsp.inlay_hint.is_enabled()))
+          end, '[T]oggle Inlay [H]ints')
+          -- end
 
           if client and client.name == 'ts_ls' then
             -- use vim.inspect to print lua-table
