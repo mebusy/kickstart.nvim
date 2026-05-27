650aa639c79db747dac2d66b7a8a29eb88494d47
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2026-05-27 15:04:05 +0800

    m

--- init.lua
+++ init.lua
@@ -294,6 +294,18 @@ vim.opt.showmode = false
 -- vim.schedule(function()
 --   vim.opt.clipboard = 'unnamedplus'
 -- end)
+-- ssh 场景，系统剪切板透传
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
 
 -- Enable break indent
 vim.opt.breakindent = true
