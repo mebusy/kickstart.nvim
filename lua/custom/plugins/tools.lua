d89de2485926ab8bc70c40e720e575fe24602502
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2026-06-24 11:10:28 +0800

    + lewis6991/gitsigns.nvim

--- lua/custom/plugins/tools.lua
+++ lua/custom/plugins/tools.lua
@@ -57,4 +57,30 @@ return {
   -- {
   --   'vim-airline/vim-airline',  -- will make editor confusing
   -- },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      current_line_blame = true, -- 在当前行末尾显示 blame 信息
+      current_line_blame_opts = {
+        delay = 200,
+        virt_text_pos = 'eol',
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary> · <abbrev_sha>',
+    },
+    config = function(_, opts)
+      require('gitsigns').setup(opts)
+
+      local function set_gitsigns_hl()
+        vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', {
+          fg = 'darkgray',
+        })
+      end
+
+      set_gitsigns_hl()
+
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        callback = set_gitsigns_hl,
+      })
+    end,
+  },
 }
